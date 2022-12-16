3286043f8954f328d1bcb0b0dde4a1f52ce7c6fc
fmilohano20 <fmilohano20@foi.hr>
2022-12-16 15:43:02 +0100

    Some code cleanup

--- Software/STONKS/DataAccessLayer/Repositories/PrimkaRepository.cs
+++ Software/STONKS/DataAccessLayer/Repositories/PrimkaRepository.cs
@@ -22,47 +22,7 @@ namespace DataAccessLayer.Repositories
             return query;
         }
 
-        public int AddTransactional(Primka primka, List<StavkaPrimke> stavke)
-        {
-            var StavkePrimke = Context.Set<StavkaPrimke>();
-            var Primke = Context.Set<Primka>();
-
-            // Create a TransactionScope to manage the transaction
-            using (var transaction = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead }))
-            {
-                try
-                {
-                    Primke.Attach(primka);
-
-                    var entry =  Primke.Add(primka);
-                    int affectedRows = Context.SaveChanges();
-                    if (affectedRows > 0)
-                    {
-                        foreach (var stavka in stavke)
-                        {
-                            stavka.primka_id = entry.id;   
-                            //stavka.Artikli = null;
-                            StavkePrimke.Attach(stavka);
-                            StavkePrimke.Add(stavka);
-                            Context.SaveChanges();
-                        }                        
-                        transaction.Complete(); // If all operations were successful, commit the transaction
-                        return 1;
-                    }
-                    return 0;
-                    
-
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                    // If an exception was thrown, roll back the transaction
-                    transaction.Dispose();
-                    return 0;
-                }
-            }
-
-        }
+        
         public override int Add(Primka entity, bool save = true)
         {
             Entities.Attach(entity);
