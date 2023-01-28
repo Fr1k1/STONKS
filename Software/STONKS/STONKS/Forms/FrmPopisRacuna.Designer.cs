eda5c30b242966e6c2e197b37089946f63f57ce0
askarica20 <askarica20@foi.hr>
2023-01-28 18:20:28 +0100

    fix scroll on popis racuna

--- Software/STONKS/STONKS/Forms/FrmPopisRacuna.Designer.cs
+++ Software/STONKS/STONKS/Forms/FrmPopisRacuna.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea9 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend9 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea10 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend10 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.cboVrsta = new System.Windows.Forms.ComboBox();
@@ -59,7 +59,8 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Azonix", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.label2.Location = new System.Drawing.Point(117, 19);
+            this.label2.Location = new System.Drawing.Point(156, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(174, 19);
             this.label2.TabIndex = 18;
@@ -70,7 +71,8 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Azonix", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(57)))), ((int)(((byte)(67)))));
-            this.label1.Location = new System.Drawing.Point(23, 19);
+            this.label1.Location = new System.Drawing.Point(31, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(102, 19);
             this.label1.TabIndex = 17;
@@ -78,27 +80,32 @@
             // 
             // cboVrsta
             // 
+            this.cboVrsta.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.cboVrsta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(188)))), ((int)(((byte)(196)))));
             this.cboVrsta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cboVrsta.Font = new System.Drawing.Font("Azonix", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cboVrsta.ForeColor = System.Drawing.SystemColors.MenuText;
             this.cboVrsta.FormattingEnabled = true;
             this.cboVrsta.ItemHeight = 21;
-            this.cboVrsta.Location = new System.Drawing.Point(802, 73);
+            this.cboVrsta.Location = new System.Drawing.Point(1069, 90);
+            this.cboVrsta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cboVrsta.Name = "cboVrsta";
-            this.cboVrsta.Size = new System.Drawing.Size(160, 29);
+            this.cboVrsta.Size = new System.Drawing.Size(212, 29);
             this.cboVrsta.TabIndex = 25;
             this.cboVrsta.Text = " filter";
             this.cboVrsta.SelectedIndexChanged += new System.EventHandler(this.cboVrsta_SelectedIndexChanged);
             // 
             // dgvRacuni
             // 
+            this.dgvRacuni.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvRacuni.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvRacuni.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
             this.dgvRacuni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRacuni.Location = new System.Drawing.Point(27, 111);
+            this.dgvRacuni.Location = new System.Drawing.Point(36, 137);
+            this.dgvRacuni.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvRacuni.Name = "dgvRacuni";
-            this.dgvRacuni.Size = new System.Drawing.Size(935, 223);
+            this.dgvRacuni.Size = new System.Drawing.Size(1247, 274);
             this.dgvRacuni.TabIndex = 26;
             this.dgvRacuni.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRacuni_CellClick);
             // 
@@ -107,7 +114,8 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Azonix", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.label3.Location = new System.Drawing.Point(23, 357);
+            this.label3.Location = new System.Drawing.Point(31, 439);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(180, 19);
             this.label3.TabIndex = 27;
@@ -115,12 +123,15 @@
             // 
             // dgvStavke
             // 
+            this.dgvStavke.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvStavke.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvStavke.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
             this.dgvStavke.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvStavke.Location = new System.Drawing.Point(27, 379);
+            this.dgvStavke.Location = new System.Drawing.Point(36, 466);
+            this.dgvStavke.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvStavke.Name = "dgvStavke";
-            this.dgvStavke.Size = new System.Drawing.Size(935, 223);
+            this.dgvStavke.Size = new System.Drawing.Size(1247, 274);
             this.dgvStavke.TabIndex = 28;
             // 
             // btnPovratak
@@ -129,9 +140,10 @@
             this.btnPovratak.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPovratak.Font = new System.Drawing.Font("Azonix", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPovratak.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(219)))), ((int)(((byte)(219)))));
-            this.btnPovratak.Location = new System.Drawing.Point(27, 880);
+            this.btnPovratak.Location = new System.Drawing.Point(36, 1083);
+            this.btnPovratak.Margin = new System.Windows.Forms.Padding(4, 4, 4, 30);
             this.btnPovratak.Name = "btnPovratak";
-            this.btnPovratak.Size = new System.Drawing.Size(129, 38);
+            this.btnPovratak.Size = new System.Drawing.Size(172, 47);
             this.btnPovratak.TabIndex = 29;
             this.btnPovratak.Text = "Povratak";
             this.btnPovratak.UseVisualStyleBackColor = false;
@@ -139,13 +151,15 @@
             // 
             // btnStorniraj
             // 
+            this.btnStorniraj.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnStorniraj.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(57)))), ((int)(((byte)(67)))));
             this.btnStorniraj.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStorniraj.Font = new System.Drawing.Font("Azonix", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnStorniraj.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(219)))), ((int)(((byte)(219)))));
-            this.btnStorniraj.Location = new System.Drawing.Point(770, 880);
+            this.btnStorniraj.Location = new System.Drawing.Point(1027, 1083);
+            this.btnStorniraj.Margin = new System.Windows.Forms.Padding(4, 4, 4, 30);
             this.btnStorniraj.Name = "btnStorniraj";
-            this.btnStorniraj.Size = new System.Drawing.Size(187, 38);
+            this.btnStorniraj.Size = new System.Drawing.Size(249, 47);
             this.btnStorniraj.TabIndex = 31;
             this.btnStorniraj.Text = "storaniraj";
             this.btnStorniraj.UseVisualStyleBackColor = false;
@@ -153,43 +167,47 @@
             // 
             // chartRacuni
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chartRacuni.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chartRacuni.Legends.Add(legend1);
-            this.chartRacuni.Location = new System.Drawing.Point(51, 617);
+            chartArea9.Name = "ChartArea1";
+            this.chartRacuni.ChartAreas.Add(chartArea9);
+            legend9.Name = "Legend1";
+            this.chartRacuni.Legends.Add(legend9);
+            this.chartRacuni.Location = new System.Drawing.Point(68, 759);
+            this.chartRacuni.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chartRacuni.Name = "chartRacuni";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series1.Legend = "Legend1";
-            series1.Name = "Broj placanja";
-            this.chartRacuni.Series.Add(series1);
-            this.chartRacuni.Size = new System.Drawing.Size(438, 244);
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series9.Legend = "Legend1";
+            series9.Name = "Broj placanja";
+            this.chartRacuni.Series.Add(series9);
+            this.chartRacuni.Size = new System.Drawing.Size(584, 300);
             this.chartRacuni.TabIndex = 38;
             this.chartRacuni.Text = "chartRacuni";
             // 
             // chartStavke
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chartStavke.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chartStavke.Legends.Add(legend2);
-            this.chartStavke.Location = new System.Drawing.Point(509, 617);
+            this.chartStavke.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea10.Name = "ChartArea1";
+            this.chartStavke.ChartAreas.Add(chartArea10);
+            legend10.Name = "Legend1";
+            this.chartStavke.Legends.Add(legend10);
+            this.chartStavke.Location = new System.Drawing.Point(679, 759);
+            this.chartStavke.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chartStavke.Name = "chartStavke";
             this.chartStavke.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Broj artikala";
-            this.chartStavke.Series.Add(series2);
-            this.chartStavke.Size = new System.Drawing.Size(424, 244);
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "Broj artikala";
+            this.chartStavke.Series.Add(series10);
+            this.chartStavke.Size = new System.Drawing.Size(565, 300);
             this.chartStavke.TabIndex = 39;
             this.chartStavke.Text = "chart1";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(327, 76);
+            this.dateTimePicker1.Location = new System.Drawing.Point(436, 94);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
             this.dateTimePicker1.TabIndex = 40;
             this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
@@ -198,7 +216,8 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Azonix", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.label4.Location = new System.Drawing.Point(23, 77);
+            this.label4.Location = new System.Drawing.Point(31, 95);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(298, 19);
             this.label4.TabIndex = 41;
@@ -210,9 +229,10 @@
             this.btnPrikaziSve.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPrikaziSve.Font = new System.Drawing.Font("Azonix", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPrikaziSve.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(219)))), ((int)(((byte)(219)))));
-            this.btnPrikaziSve.Location = new System.Drawing.Point(484, 72);
+            this.btnPrikaziSve.Location = new System.Drawing.Point(645, 89);
+            this.btnPrikaziSve.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPrikaziSve.Name = "btnPrikaziSve";
-            this.btnPrikaziSve.Size = new System.Drawing.Size(154, 29);
+            this.btnPrikaziSve.Size = new System.Drawing.Size(205, 36);
             this.btnPrikaziSve.TabIndex = 42;
             this.btnPrikaziSve.Text = "prikazi sve";
             this.btnPrikaziSve.UseVisualStyleBackColor = false;
@@ -225,10 +245,11 @@
             // 
             // FrmPopisRacuna
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
-            this.ClientSize = new System.Drawing.Size(983, 948);
+            this.ClientSize = new System.Drawing.Size(1311, 1197);
             this.Controls.Add(this.btnPrikaziSve);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dateTimePicker1);
@@ -243,6 +264,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmPopisRacuna";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Popis racuna";
